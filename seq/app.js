f096c3defba05ff8ecb8d6831ff0d5be7f3484db
Anuj <anujsaheba@gmail.com>
2019-01-22 13:36:21 -0600

    updated

--- seq/app.js
+++ seq/app.js
@@ -4,13 +4,21 @@ var session = require('express-session'); // related to session; npm install exp
 var cookie_parser = require('cookie-parser'); // related to cookie-parser; npm install cookie-parser
 var db = require('./models'); // related to sequelize and directed to index file ; npm install sequelize (refer to documentation for additional package installation)
 
+const SequelizeSessionStore = require('sequelize-session-store')(session); // npm install sequelize-session-store
+
+var myStore = new SequelizeSessionStore({
+    db:db
+});
+
+
+app.use(cookieParser();)
+
 app.use(session({ // to use, need to require session
     secret: 'some-special-secret',
     resave: false,
     proxy: true,
     store: myStore // to utilize need to do npm install sequelize-session-store
 }));
-
 
 
 app.listen(4000, ()=>{
